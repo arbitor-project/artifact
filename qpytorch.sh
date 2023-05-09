6e50527f1fb0c3dd98eab81fb5d6b98a50e71667
Chenhao Jiang <jiangchenhao2@Outlook.com>
2023-05-09 00:35:14 -0400

    Address problem that qtorch does not work with newer torch

--- qpytorch.sh
+++ qpytorch.sh
@@ -1,4 +1,4 @@
 python -m venv pytorch-cifar/venv
 source pytorch-cifar/venv/bin/activate
-pip install qtorch torchvision --quiet
+pip install qtorch torch==1.8.1 torchvision==0.9.1 --quiet
 python pytorch-cifar/main.py
